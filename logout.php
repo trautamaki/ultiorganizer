c3252f6912d66cdbbda8caff22c081054a95484f
Timi Rautamäki <timi.rautamaki@gmail.com>
2023-02-26 13:01:43 +0000

    Convert logout.php to Smarty

--- logout.php
+++ logout.php
@@ -1,23 +1,6 @@
 <?php
 $title = _("Logout");
-$html = "";
+$smarty->assign("title", $title);
 
 ClearUserSessionData();
 
-$html .= "<h1>" . _("You have logged out") . "</h1>";
-
-if (IsFacebookEnabled()) {
-	$html .= "<script type=\"text/javascript\">
-<!--
-window.onload = function() {
-	FB.getLoginStatus(function(response) {
-	  	if (response.session) {
-	  		FB.logout(function(loresp) {});
-		}
-	});
-};
-//-->
-</script>";
-}
-
-showPage($title, $html);
